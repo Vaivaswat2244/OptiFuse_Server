d0ee8b463ec77b9ea5136bc3cf6156c0390188a6
Vaivaswat <vaivaswat2244@gmail.com>
2025-08-16 16:42:03 +0530

    added authtokens

--- backend/settings.py
+++ backend/settings.py
@@ -40,8 +40,10 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'rest_framework',
+    'rest_framework.authtoken',
     'corsheaders',
     'core.apps.CoreConfig',
+    'simulation.apps.SimulationConfig',
 ]
 
 MIDDLEWARE = [
@@ -88,7 +90,7 @@ DATABASES = {
         default=config('DATABASE_URL')
     )
 }
-
+DATABASES['default']['OPTIONS'] = {'sslmode': 'require'}
 # Password validation
 # https://docs.djangoproject.com/en/5.2/ref/settings/#auth-password-validators
 
@@ -129,3 +131,13 @@ STATIC_URL = 'static/'
 # https://docs.djangoproject.com/en/5.2/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+REST_FRAMEWORK = {
+    'DEFAULT_AUTHENTICATION_CLASSES': [
+        'rest_framework.authentication.TokenAuthentication',
+    ],
+    'DEFAULT_PERMISSION_CLASSES': [
+        # By default, all endpoints will require authentication
+        'rest_framework.permissions.IsAuthenticated',
+    ]
+}
